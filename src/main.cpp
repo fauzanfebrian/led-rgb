84687dd17c1031c6e4b3a5855937f30f25159c22
fauzanfebrian <fauzanfebriann@gmail.com>
2024-06-26 21:21:17 +0700

    listen to serial

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,31 @@
 #define GREEN_PIN 10
 #define BLUE_PIN 11
 
+#define LED_PIN 2
+
 #define MIC_PIN A0
 
 void setup()
 {
     Serial.begin(9600);
+    while (!Serial)
+    {
+        ; // Wait for the serial port to connect. Needed for native USB port only
+    }
+    Serial.println("Serial communication started.");
+
     // set the pins as output
     pinMode(MIC_PIN, INPUT);
 
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
+
+    Serial.println("RGB LED is ready");
 }
+
+String inputString = "";
+bool stringComplete = false;
 
 void loop()
 {
@@ -25,5 +38,42 @@ void loop()
     // set the color of the LED
     setRGBValue(micValue, RED_PIN, GREEN_PIN, BLUE_PIN);
 
-    delay(100);
+    if (stringComplete)
+    {
+        inputString.trim();
+        if (inputString == "on")
+        {
+            analogWrite(LED_PIN, 255);
+            Serial.println("LED is on");
+        }
+        else if (inputString == "off")
+        {
+            analogWrite(LED_PIN, 0);
+            Serial.println("LED is off");
+        }
+
+        Serial.println(inputString);
+        inputString = "";
+        stringComplete = false;
+    }
+}
+
+void serialEvent()
+{
+    while (Serial.available())
+    {
+        // Get the new byte
+        char inChar = (char)Serial.read();
+
+        if (inChar == '\n')
+        {
+            Serial.println("Enter");
+            stringComplete = true;
+        }
+        else
+        {
+            inputString += inChar;
+            Serial.print(inChar);
+        }
+    }
 }
